e0ce302f6c01a4755676f0df266b34d9d2532317
Sage-boy <118199396+Sage-boy@users.noreply.github.com>
2022-11-14 21:19:45 +0530

    Add files via upload

---
+++ extra-file/responses.js
@@ -0,0 +1,47 @@
+    function getBotResponse(input) {
+    //rock paper scissors
+    if(input == "rock") {return "paper";} else if (input == "paper") {return "scissors";} else if (input == "scissors") { return "rock";} 
+    // => Natural resources
+    if (input == "How you create these type of art work?"){return "Just do practice. ";} else if (input == "what is yur age"){return "16!";} 
+    if (input == "Do you have any girlfriend"){return "Not now. ";} else if (input == "do you have any girlfriend"){return "Not now. ";}
+    // => Normal resource//
+    if (input == "How do you manage your programming as well as studies?"){
+    return "Its so hard to tell,but due to my curiosity of programming i will have to manage my studies as well as programming.";}
+    else if (input == "how do you manage your studies as well as programming?"){return "Its so hard to tell,but due to my curiosity of programming i will have to manage my studies as well as programming.";
+    }else if (input == "how to become a good programmer?"){return "You can learn it from youtube as well as from any programming course.";} 
+    else if (input == "what's your future plan"){return "this my friend";}if (input == "hey"){return "hello";}else if (input == "hii"){return "hello";
+    } else  if (input == "hey there"){return "hello";} else  if (input == "hello bro!"){return "hello";} else  if (input == "nice to meet you"){return "i also!";}
+    // => Greetings resources
+    if (input == "Happy birthday"){return "Same to you ! Thank you";} else if(input == "Happy diwali"){
+    return "Same to you ! Thank you"} else if (input == "happy holi"){return "Same to you ! Thank you";} else if (input == "Happy janmastmi"){
+    return "Same to you ! Thank you";} else if (input == "Happy dasharah"){return "Same to you ! Thank you";}else if (input == "Happy gudipadva"){return "Same to you ! Thank you";
+    } else if (input == "Happy christmas"){return "Same to you ! Thank you";} else if (input == "Happy new year"){return "Same to you ! Thank you";}
+    // => Joking/Bad things resources
+    if (input == "what? chatting"){return "No! I mean programming";} else if (input == "bitch"){return "Same to you,Madafucka";} 
+    else if (input == "Fuck you bitch"){return "Same to you! Fucking bitch ";} 
+    // => critical resources 
+    if (input == "is your are sachin's assistant?"){return "NO! i am his visual copy.";} else if (input == "By which language do you created?"){
+    return "I am created by Html/css and java script.";} else if (input == "Who is your creator?"){
+    return "I was created by sachin chaurasiya, who is the creator and owner of this website.";} else if (input == "when he had created you?")
+    {return "He had created me on 26th september 2022 and the day was monday.";
+    } else if (input == "How many program file having in this website?"){return "This website having 25 program file with 1000+ codes";} 
+    else if (input == "How you created?"){return "I also created by html/css and java script and the whole website is created in vs code";
+    } else if (input == "Does any other person create you or you also created by your owner?"){return "Yes you are are right i also created by my owner.";} 
+    else if (input == "does you are the one or you have partner in your work"){return "i am the only one.and i dont have any working partner";
+    } else if(input == "Do you tell your own thoughts"){return "No! Everything i tell is my owners thoughts.";}
+    //remaining resource 
+    if (input == "How are you"){return "I am fine, Thank you.";} else if (input == "How are you?"){return "I am fine, Thank you.";} 
+    else if (input == "Do you linked with your owner?"){return "No! i am not";}
+    // => Resources related to study  
+    if(input == "In which standerd do you studying now?"){return "I am studying in 10th std."}else if (input == "How many percentage do you want in board exam?")
+    {return "According to my study, my approximate gole is more then 80%"}
+    //Interview type resources
+    if (input == "Why do you stop photoshopping?"){return "Because i realised that programming is my real talent and photoshop is only just my hobby.";
+    } else if (input == "when did you realised that?"){
+    return "Sorry that thing i can't tell you.";}if (input == "When did you know about photoshop?"){
+    return "When i was 14 years old my parent given me a pc in gift and by millions of search i found photoshop but unfortunately i couldn't make it my profession."}
+    if (input == "What you are going to do next?"){return"I am going to learn new programming language";}
+    // => Simple responses
+    if (input == "hello!") {return "Hello there!";} else if (input == "goodbye") {return "Talk to you later!";}
+     else {return "Something went wrong,check your grammetical mistake or something else";} 
+    } 
